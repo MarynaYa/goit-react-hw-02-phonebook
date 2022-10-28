0154a4a50bcd686c3a62c48b571c0f9c650d46b6
MarynaYa <103567926+MarynaYa@users.noreply.github.com>
2022-10-28 15:32:23 +0300

    phonobooks-commit

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import s from './Section.module.css';
 
-const Section = ({ title, children }) => (
-  <section>
-    <h2>{title}</h2>
+function Section ({ title, children }) {
+ return <section className={s.section}>
+    <h2 className={s.titleSection}>{title}</h2>
     {children}
   </section>
-);
+};
 
 Section.propTypes = {
   title: PropTypes.string.isRequired,
-  children: PropTypes.object.isRequired,
+  children: PropTypes.node,
 };
 
 export default Section;
